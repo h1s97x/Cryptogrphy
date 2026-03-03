cb3f2eae2791abf1c721ef6bf14c31ac29181c12
H1S97X <Yang1297656998@outlook.com>
2026-03-03 10:41:37 +0800

    阶段3完成：删除所有旧目录结构和兼容层文件

--- MathematicalBasis.py
+++
@@ -1,4 +0,0 @@
-# Temporary import module for MathematicalBasis - imports from new structure
-from ui.widgets.CRT_ui import CRTWidget
-from ui.widgets.Euclidean_ui import EuclideanWidget
-from ui.widgets.Euler_ui import EulerWidget
--- StreamCipher.py
+++
@@ -1,5 +0,0 @@
-# Temporary import module for StreamCipher - imports from new structure
-from ui.widgets.Crypto_1_ui import Crypto1Widget
-from ui.widgets.RC4_ui import RC4Widget
-from ui.widgets.SEAL_ui import SEALWidget
-from ui.widgets.ZUC_ui import ZUCWidget
